a17d1b17e17d64f5ba2e425a289f05e54e4a50a2
Kerry Xing <kxing@mit.edu>
2012-03-01 14:15:18 -0500

    Made the admin better for viewing and editing User data.

--- data/admin.py
+++ data/admin.py
@@ -1,6 +1,19 @@
-from data.models import User
-from data.models import Item
+from data.models import User, Category, Item, Filter, Claim
 from django.contrib import admin
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['username']}),
+        ('Personal Information',
+            {'fields': ['first_name', 'last_name', 'email', 'cell_phone']}
+        ),
+    ]
+    list_display = ['username', 'first_name', 'last_name', 'email', 'cell_phone']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Filter)
+admin.site.register(Claim)
+
