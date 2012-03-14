aab60a9e4a353c1fe4c7c86f4f98c295b477f286
Kerry Xing <kxing@mit.edu>
2012-03-13 20:34:14 -0400

    Cleaned up a test.

--- data/tests.py
+++ data/tests.py
@@ -6,15 +6,24 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from data.models import User
+from data.models import User, Category
 
 class UserTest(TestCase):
-    def test_users(self):
-        """
-        Tests to make sure that users can be created, deleted, and queried.
-        """
+    def setUp(self):
         # create a user
         User.create_user('asdf1234', 'Asdf', 'Qwerty', 'asdf1234@mit.edu', '(123)456-7890')
+
+    def tearDown(self):
+        u = User.objects.get(username='asdf1234')
+
+        # delete the user
+        User.delete_user(u)
+
+    def test_user(self):
+        """
+        Tests to make sure that users can be queried, and that their attributes are correct.
+        """
+        # create a user
 
         # find the user and check it
         u = User.get_user('asdf1234')
@@ -24,9 +33,8 @@ class UserTest(TestCase):
         self.assertEqual(u.email, 'asdf1234@mit.edu')
         self.assertEqual(u.cell_phone, '(123)456-7890')
 
-        # delete the user
-        User.delete_user(u)
-
-        # check that the user isn't there anymore
-        self.assertRaises(User.DoesNotExist, User.get_user, 'kxing')
-
+'''
+class CategoryTest(TestCase):
+    def test_categories(self):
+        self.assertEqual
+'''
