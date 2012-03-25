203cf747c5af1716c27d1f776765b4c60c7bedbf
pH14 <pwh@mit.edu>
2012-03-25 14:48:00 -0700

    Added in (super rough) tastypie API

---
+++ api/resources.py
@@ -0,0 +1,61 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+from Geddit.data.models import User, Claim, Filter, Category, Item
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email']
+        filtering = {
+            "username": ALL,
+        }
+
+class ItemResource(ModelResource):
+    #category = fields.ForeignKey(CategoryResource, 'category')
+    seller_user = fields.ForeignKey(UserResource, 'seller_user')
+    
+    class Meta:
+        queryset = Item.objects.all()
+        resource_name = 'item'
+        excludes = ['resource_uri']
+
+        filtering = {
+            "name": ALL,
+            "category": ALL_WITH_RELATIONS,
+            "seller_user": ALL_WITH_RELATIONS
+        }
+        
+class ClaimResource(ModelResource):
+    buyer = fields.ForeignKey(UserResource, 'user')
+    item = fields.ForeignKey(ItemResource, 'item')
+    
+    class Meta:
+        queryset = Claim.objects.all()
+        resource_name = 'claim'
+        filtering = {
+            "buyer": ALL_WITH_RELATIONS,
+            "item": ALL_WITH_RELATIONS,
+        }
+
+class CategoryResource(ModelResource):
+    items = fields.ToManyField(ItemResource,
+        attribute=lambda bundle: Item.objects.filter(category=bundle.obj))
+    
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'category'
+        excludes = ['resource_uri']
+        filtering = {
+            "name": ALL_WITH_RELATIONS,
+        }
+
+class FilterResource(ModelResource):
+    user = fields.ToOneField(UserResource, 'user')
+    class Meta:
+        queryset = Filter.objects.all()
+        resource_name = 'filter'
+        filtering = {
+            "user": ALL_WITH_RELATIONS,
+        }
--- urls.py
+++ urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from tastypie.api import Api
+from Geddit.api.resources import UserResource, ItemResource, ClaimResource, CategoryResource, FilterResource
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(ClaimResource())
+v1_api.register(ItemResource())
+v1_api.register(CategoryResource())
+v1_api.register(FilterResource())
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,6 +23,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^jstest/', 'jstest.views.index'),
+    url(r'^api/', include(v1_api.urls)),
     url(r'', 'data.views.index')
 )
