14294ed2fc7f7ec8b9f51a389afa7ab18635ee75
Kerry Xing <kxing@mit.edu>
2012-03-27 20:53:46 -0400

    Added a CSS file, and added more to the site skeleton.

--- data/views.py
+++ data/views.py
@@ -1,11 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from django.contrib.auth.views import login
 
 #from .auth import scripts_login
 
 def index(request):
     t = loader.get_template('index.html')
-    c = Context()
+    c = RequestContext(request)
     return HttpResponse(t.render(c))
