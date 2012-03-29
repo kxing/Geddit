4f2c9ec764ec908caf615db01016c62cab61c05d
Kerry Xing <kxing@mit.edu>
2012-03-29 08:37:43 -0400

    Added a sidebar for browsing by category.

--- data/views.py
+++ data/views.py
@@ -1,11 +1,11 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
-from django.contrib.auth.views import login
+from data.models import Category
 
 #from .auth import scripts_login
 
 def index(request):
     t = loader.get_template('index.html')
-    c = RequestContext(request)
+    c = RequestContext(request, {'categories': Category.get_all_categories()})
     return HttpResponse(t.render(c))
