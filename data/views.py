09861c2f6a7fa3de363e590d837baf6dbf2bde20
Kerry Xing <kxing@mit.edu>
2012-04-02 18:53:41 -0400

    Added the items to the home page.

--- data/views.py
+++ data/views.py
@@ -1,11 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader, RequestContext
-from data.models import Category
+from data.models import Category, Item
 
 #from .auth import scripts_login
 
 def index(request):
     t = loader.get_template('index.html')
-    c = RequestContext(request, {'categories': Category.get_all_categories()})
+    c = RequestContext(request, \
+            {'categories': Category.get_all_categories(),
+            'items' : Item.get_all_items()
+            })
     return HttpResponse(t.render(c))
