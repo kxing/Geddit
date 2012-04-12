75fb51bb3ce6b226ffe3c0d3f3b8a0bbfce6bbd6
Kerry Xing <kxing@mit.edu>
2012-04-12 19:42:01 -0400

    Made the cancel button work on the form.

--- data/static/js/reserve.js
+++ data/static/js/reserve.js
@@ -2,3 +2,7 @@ function showNewReservationDialog() {
     $(".greyout-visible").css("visibility", "visible");
 }
 
+function hideNewReservationDialog() {
+    $(".greyout-visible").css("visibility", "hidden");
+}
+
